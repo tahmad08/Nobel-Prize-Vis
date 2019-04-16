4d0e3ad6520041bcc62fc2d3ad922e9b13e6a35b
tahmad08 <42183137+tahmad08@users.noreply.github.com>
2019-04-15 22:45:36 -0400

    added in the timeline chart

--- main.js
+++ main.js
@@ -1,87 +1,74 @@
+//width0 and height0 are for the actual svg
+var width0 = 1300;
+var height0 = 1100;
 
-// **** Your JavaScript code goes here ****
-var data_arr;
-var svg = d3.select("#main").select("svg");
-var width = 760;
-var height = 600;
-d3.csv("nobel_laureates.csv", function(dataset){
-  data_arr = dataset;
-  categoryPlot(data_arr);
-  //plot2(data_arr);
-  //plot1(data_arr);
+//width and height are for the graph to use itself. 
+//i could do margins? but nah
+var width = 1200;
+var height = 400;
+//made the x 
+var x = d3.scaleLinear()
+    .rangeRound([0, width])
+    .domain([1895, 2018]);
 
+var chart0G = d3.select("#chart0")
+    .append("svg:svg")
+    .attr("width", width0)
+    .attr("height", height0)
+    .append("g")
+    .attr("transform",
+                `translate(50,350)`);
 
-});
+var t = d3.transition()
+    .duration(1000);
 
-function categoryPlot(data_arr) {
+var shift = 1;
+var dataFile = "nobel_laureates.csv"
 
-    //setting up chart container
-    var chart1G = d3.select("#chart1")
-    	                .append("svg:svg")
-    	                .attr("width",width)
-    	                .attr("height",height)
-                        .append('g');
+//number of bins for histogram
+const nbins = 59;
 
-    //rollup of nobel categories
-    var category = d3.nest()
-        .key(function (d) {return d.category;})
-        .rollup(function(v) { return v.length; })
-        .entries(data_arr)
-        console.log(JSON.stringify(category)); //prints the arrays with values
+    //get data (?)
+    d3.csv(dataFile, function(error, allData) {
+        allData.forEach(function(d) {
+            d.fullname = d.fullname
+            d.year = +d.year;
+        });
 
-    //setting up axies of the chart
-    var x = d3.scaleBand()
-        .domain(category.map(function (d) { return d.key }))
-        .range([0, width/2 - 100])
+        //histogram binning
+        const histogram = d3.histogram()
+            .domain(x.domain())
+            .thresholds(x.ticks(nbins))
+            .value(d => +d.year);
 
+        //binning data and filtering out empty bins
+        const bins = histogram(allData);
+            console.log(bins)
 
-    var extent = d3.extent(category, function(d) {return d.value})
-
-
-    var y = d3.scaleLinear()
-        .domain([0, extent[1] + 10])
-        .range([height, 300])
-    //setting up the colors
-    function colorPicker(cat) {
-        if (cat == "chemistry") {
-            return "#f2db48" //minion yellow
-        } else if (cat == "literature") {
-            return "#c97064" //red
-        } else if (cat == "medicine") {
-            return "#bca371" //wood brown
-        } else if (cat == "peace") {
-            return "#a6b07e" //sand (sage)
-        } else if (cat == "physics") {
-            return "#68a357" //russian green
-        } else if (cat == "economics") {
-            return "#32965d"  //sea green
-        }
-        return "#ffffff";
-    }
-
-
-    //drawing the bars
-    chart1G.selectAll()
-        .data(category)
+        //g container for each bin
+        let binContainer = chart0G.selectAll("g.gBin")
+        .data(bins)
         .enter()
-        .append("rect")
-        .attr("class", "bar")
-        .attr("x", function (d) { return x(d.key) + 50})
-        .attr("y", function(d) {return y(d.value) - 50})
-        .attr("width", 20)
-        .attr("height", function (d) { return height - y(d.value) })
-        //coloring the bar
-        .attr("fill", function (d) {return colorPicker(d.key) } )
+        .append("g" )
+        .attr("class", "gBin")
+        .attr("transform", d => `translate(${x(d.x0)}, ${20})`)
+        .selectAll("circle")
+        .data(d => d.map((p, i) => {
+            return {value: p.year,
+                    radius: (x(d.x1)-x(d.x0))/4.5}
+        }))
+        .enter()
+        .append("circle")
+        .attr("class", "enter")
+        .attr("cx", 0) //g element already at correct x pos
+        .attr("cy", (d, i) => {
+            return -1*(- i * 2.3 * d.radius - d.radius)})
+        .attr("r", d => d.radius);
+        // add x axis
+        chart0G.append("g")
+            .attr("class", "axis axis--x")
+            .attr("transform", "translate(0," + 20 + ")")
+            .call(d3.axisTop(x));
 
+    });
 
-    chart1G.append('g').attr('class', 'xaxis')
-            .attr("transform", "translate (40, 560)")
-            .call(d3.axisBottom(x))
-
-    chart1G.append('g').attr('class', 'y axis')
-            .attr("transform", "translate (40,-50)")
-            .call(d3.axisLeft(y))
-}
-
-//TODO build the timeline and link it to category (probably need lots of TA help for this)
-//TODO set up the age ranges and then make a plot
