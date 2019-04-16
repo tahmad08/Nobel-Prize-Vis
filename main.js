4a885d38957d7cf2a3a5e412f80e3466623390fb
tahmad08 <42183137+tahmad08@users.noreply.github.com>
2019-04-16 07:55:27 -0400

    fixed the overlapping svg issue

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ var height0 = 800;
 var width = 1250;
 var height = 400;
 
+
 //made the x 
 var x = d3.scaleLinear()
     .rangeRound([0, width])
@@ -47,7 +48,6 @@ d3.csv(dataFile, function(error, allData) {
         d.year = +d.year
         d.motivation = d.motivation;
     });
-    categoryChart();
     //histogram binning
     const histogram = d3.histogram()
         .domain(x.domain())
@@ -106,8 +106,10 @@ d3.csv(dataFile, function(error, allData) {
             .attr("transform", "translate(0," + 20 + ")")
             .call(d3.axisTop(x));
 
-});
 
+});
+categoryChart();
+chart0G.call(tooltip);
 //hover functionality
 function tooltipon(d){
     let gParent = d3.select(this.parentElement)
@@ -137,105 +139,100 @@ function tooltipoff(d) {
            .style("opacity", 0);
   }//tooltipOff
 
+  
 function categoryChart(){
-    
-// **** Your JavaScript code goes here ****
-var data_arr;
-var svg = d3.select("#main").select("svg");
-var width = 760;
-var height = 600;
-d3.csv("nobel_laureates.csv", function(dataset){
-    data_arr = dataset;
-    categoryPlot(data_arr);
+    // **** Your JavaScript code goes here ****
+    var data_arr;
+    var svg = d3.select("#main").select("svg");
+    var width0 = 325;
+    var height0 = 375;
+    var width = 760;
+    var height = 600;
+    d3.csv("nobel_laureates.csv", function(dataset){
+        data_arr = dataset;
+        categoryPlot(data_arr);
 
-});
+    });
 
+    function categoryPlot(data_arr) {
 
+        //setting up chart container
+        var chart1G = d3.select("#chart1")
+                            .append("svg:svg")
+                            .attr("width",width0)
+                            .attr("height",height0)
+                            .append('g');
 
-function categoryPlot(data_arr) {
+        //rollup of nobel categories
+        var category = d3.nest()
+            .key(function (d) {return d.category;})
+            .rollup(function(v) { return v.length; })
+            .entries(data_arr)
+            console.log(JSON.stringify(category)); //prints the arrays with values
 
-    //setting up chart container
-    var chart1G = d3.select("#chart1")
-    	                .append("svg:svg")
-    	                .attr("width",width)
-    	                .attr("height",height)
-                        .append('g');
-
-    //rollup of nobel categories
-    var category = d3.nest()
-        .key(function (d) {return d.category;})
-        .rollup(function(v) { return v.length; })
-        .entries(data_arr)
-        console.log(JSON.stringify(category)); //prints the arrays with values
-
-    //setting up axes of the chart
-    var x = d3.scaleBand()
-        .domain(category.map(function (d) { return d.key }))
-        .range([0, width/2 - 100])
-
-
-    var extent = d3.extent(category, function(d) {return d.value})
+        //setting up axes of the chart
+        var x = d3.scaleBand()
+            .domain(category.map(function (d) { return d.key }))
+            .range([0, width/2 - 100])
 
 
-    var y = d3.scaleLinear()
-        .domain([0, extent[1] + 10])
-        .range([height, 300])
-    //setting up the colors
-    function colorPicker(cat) {
-        if (cat == "chemistry") {
-            return "#f2db48" //minion yellow
-        } else if (cat == "literature") {
-            return "#c97064" //red
-        } else if (cat == "medicine") {
-            return "#bca371" //wood brown
-        } else if (cat == "peace") {
-            return "#a6b07e" //sand (sage)
-        } else if (cat == "physics") {
-            return "#68a357" //russian green
-        } else if (cat == "economics") {
-            return "#32965d"  //sea green
+        var extent = d3.extent(category, function(d) {return d.value})
+
+
+        var y = d3.scaleLinear()
+            .domain([0, extent[1] + 10])
+            .range([height, 300])
+        //setting up the colors
+        function colorPicker(cat) {
+            if (cat == "chemistry") {
+                return "#f2db48" //minion yellow
+            } else if (cat == "literature") {
+                return "#c97064" //red
+            } else if (cat == "medicine") {
+                return "#bca371" //wood brown
+            } else if (cat == "peace") {
+                return "#a6b07e" //sand (sage)
+            } else if (cat == "physics") {
+                return "#68a357" //russian green
+            } else if (cat == "economics") {
+                return "#32965d"  //sea green
+            }
+            return "#ffffff";
         }
-        return "#ffffff";
-    }
 
 
-    //drawing the bars
-    chart1G.selectAll()
-        .data(category)
-        .enter()
-        .append("rect")
-        .attr("class", "bar")
-        .attr("x", function (d) { return x(d.key) + 50})
-        .attr("y", function(d) {return y(d.value) - 275})
-        .attr("width", 20)
-        .attr("height", function (d) { return height - y(d.value) })
-        //coloring the bar
-        .attr("fill", function (d) {return colorPicker(d.key) } )
-    chart1G.append('g').attr('class', 'xaxis')
-            .attr("transform", "translate (40, 325)")
-            .call(d3.axisBottom(x))
+        //drawing the bars
+        chart1G.selectAll()
+            .data(category)
+            .enter()
+            .append("rect")
+            .attr("class", "bar")
+            .attr("x", function (d) { return x(d.key) + 50})
+            .attr("y", function(d) {return y(d.value) - 275})
+            .attr("width", 20)
+            .attr("height", function (d) { return height - y(d.value) })
+            //coloring the bar
+            .attr("fill", function (d) {return colorPicker(d.key) } )
+        chart1G.append('g').attr('class', 'xaxis')
+                .attr("transform", "translate (40, 325)")
+                .call(d3.axisBottom(x))
 
-    chart1G.append('g').attr('class', 'y axis')
-            .attr("transform", "translate (40, -275)")
-            .call(d3.axisLeft(y))
-
-
-     //setting up axes of the chart
-    var x0 = d3.scaleBand()
-        .domain(category.map(function (d) { return d.key }))
-        .range([0, width/2 - 100])
+        chart1G.append('g').attr('class', 'y axis')
+                .attr("transform", "translate (40, -275)")
+                .call(d3.axisLeft(y))
 
 
-    var extent0 = d3.extent(category, function(d) {return d.value})
-
-    var y0 = d3.scaleLinear()
-        .domain([0, extent0[1] + 10])
-        .range([height, 300])
-
-
-}
+        //setting up axes of the chart
+        var x0 = d3.scaleBand()
+            .domain(category.map(function (d) { return d.key }))
+            .range([0, width/2 - 100])
 
 
+        var extent0 = d3.extent(category, function(d) {return d.value})
+
+        var y0 = d3.scaleLinear()
+            .domain([0, extent0[1] + 10])
+            .range([height, 300])
 
 
-  }
+}}
