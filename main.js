768faca06654a20b27ef8085c98eba5c9bb3a6a5
tahmad08 <42183137+tahmad08@users.noreply.github.com>
2019-04-15 22:59:52 -0400

    hovering makes points green

--- main.js
+++ main.js
@@ -6,47 +6,51 @@ var height0 = 1100;
 //i could do margins? but nah
 var width = 1200;
 var height = 400;
+
 //made the x 
 var x = d3.scaleLinear()
     .rangeRound([0, width])
     .domain([1895, 2018]);
 
+//create the svg and append it, translated halfway down the screen
 var chart0G = d3.select("#chart0")
     .append("svg:svg")
     .attr("width", width0)
     .attr("height", height0)
     .append("g")
     .attr("transform",
-                `translate(50,350)`);
+                `translate(50,400)`);
 
 var t = d3.transition()
     .duration(1000);
 
-var shift = 1;
+//data file
 var dataFile = "nobel_laureates.csv"
 
 //number of bins for histogram
+// 2018 - 1900 = 118/2 = 59
 const nbins = 59;
 
-    //get data (?)
-    d3.csv(dataFile, function(error, allData) {
-        allData.forEach(function(d) {
-            d.fullname = d.fullname
-            d.year = +d.year;
-        });
+//get data
+d3.csv(dataFile, function(error, allData) {
+    //append what we need to the array allDate
+    allData.forEach(function(d) {
+        d.fullname = d.fullname
+        d.year = +d.year;
+    });
 
-        //histogram binning
-        const histogram = d3.histogram()
-            .domain(x.domain())
-            .thresholds(x.ticks(nbins))
-            .value(d => +d.year);
+    //histogram binning
+    const histogram = d3.histogram()
+        .domain(x.domain())
+        .thresholds(x.ticks(nbins))
+        .value(d => +d.year);
 
-        //binning data and filtering out empty bins
-        const bins = histogram(allData);
-            console.log(bins)
+    //binning data and filtering out empty bins
+    const bins = histogram(allData);
+        console.log(bins)
 
-        //g container for each bin
-        let binContainer = chart0G.selectAll("g.gBin")
+    //g container for each bin
+    let binContainer = chart0G.selectAll("g.gBin")
         .data(bins)
         .enter()
         .append("g" )
@@ -63,12 +67,20 @@ const nbins = 59;
         .attr("cx", 0) //g element already at correct x pos
         .attr("cy", (d, i) => {
             return -1*(- i * 2.3 * d.radius - d.radius)})
-        .attr("r", d => d.radius);
-        // add x axis
+        .attr("r", d => d.radius)
+        .on("mouseover", function(){
+            
+            d3.select(this)
+                .style("fill", "green");
+        }).on('mouseout', function () {
+            d3.select(this)
+                .style("fill", "lightblue");
+        });
+        //add x axis on top
         chart0G.append("g")
             .attr("class", "axis axis--x")
             .attr("transform", "translate(0," + 20 + ")")
             .call(d3.axisTop(x));
 
-    });
+});
 
